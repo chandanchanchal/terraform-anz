bddd03a073b87a0ad78fee83066b15a377ad4476
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2025-04-15 10:51:25 +0530

    Update c3-security-groups.tf

--- 07-Terraform-State/07-01-Terraform-Remote-State-Storage-and-Locking/terraform-manifests/c3-security-groups.tf
+++ 07-Terraform-State/07-01-Terraform-Remote-State-Storage-and-Locking/terraform-manifests/c3-security-groups.tf
@@ -1,6 +1,6 @@
 # Create Security Group - SSH Traffic
 resource "aws_security_group" "vpc-ssh" {
-  name        = "cvpc-ssh"
+  name        = "ccvpc-ssh"
   description = "Dev VPC SSH"
   ingress {
     description = "Allow Port 22"
@@ -20,7 +20,7 @@ resource "aws_security_group" "vpc-ssh" {
 
 # Create Security Group - Web Traffic
 resource "aws_security_group" "vpc-web" {
-  name        = "cvpc-web"
+  name        = "ccvpc-web"
   description = "Dev VPC web"
   ingress {
     description = "Allow Port 80"
