b604280798456071bf0675a54bb63f57f9670eda
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2025-04-15 08:31:14 +0530

    Update c2-variables.tf

--- 05-Terraform-Variables/05-01-Terraform-Input-Variables/v1-Input-Variables-Basic/c2-variables.tf
+++ 05-Terraform-Variables/05-01-Terraform-Input-Variables/v1-Input-Variables-Basic/c2-variables.tf
@@ -2,13 +2,13 @@
 variable "aws_region" {
   description = "Region in which AWS resources to be created"
   type        = string
-  default     = "us-east-1"
+  default     = "us-east-2"
 }
 
 variable "ec2_ami_id" {
   description = "AMI ID"
   type        = string
-  default     = "ami-0915bcb5fa77e4892" # Amazon2 Linux AMI ID
+  default     = "ami-0100e595e1cc1ff7f" # Amazon2 Linux AMI ID
 }
 
 variable "ec2_instance_count" {
